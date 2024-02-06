694c6265b0f5b31ea412d7d22ddc0bcf2dde9b90
Mateus Borba <mateus.neg25@gmail.com>
2024-02-06 01:09:56 -0300

    Melhorias no metodo imprimirTabuleiro

--- Xadrez_Console/Tela.cs
+++ Xadrez_Console/Tela.cs
@@ -9,6 +9,7 @@ namespace Xadrez_Console
         {
             for (int i=0; i<tab.Linhas; i++)
             {
+                Console.Write(8-i+ " ");
                 for (int j=0; j<tab.Colunas; j++)
                 {
                     if (tab.peca(i,j)==null)
@@ -17,13 +18,29 @@ namespace Xadrez_Console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca (tab.peca(i,j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  A B C D E F G H");
         }
        
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.Cor== Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
     }
 }
