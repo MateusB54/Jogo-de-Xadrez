44e152cb4643df9276d0db63b977d52d1b2ab830
Mateus Borba <mateus.neg25@gmail.com>
2024-02-12 00:49:31 -0300

    Execução de testes de movimentos possiveis das peças

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -20,6 +20,13 @@ namespace Xadrez_Console
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao Origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis =partida.Tab.peca(Origem).movimentosPossiveis();
+
+                    Console.Clear() ;
+                    Tela.imprimirTabuleiro(partida.Tab, posicoesPossiveis) ;
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao Destino = Tela.lerPosicaoXadrez().toPosicao();
                     partida.executarMovimento (Origem, Destino);
